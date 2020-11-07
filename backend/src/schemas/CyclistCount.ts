e8ce5f845bd8a24a147516071c52c1b1301a5ad3
Igor Matos <igormspc@gmail.com>
2020-11-07 19:56:54 -0300

    Add men count property on schema

--- backend/src/schemas/CyclistCount.ts
+++ backend/src/schemas/CyclistCount.ts
@@ -321,6 +321,24 @@ const CyclistCountSchema = new mongoose.Schema({
           19: Number,
         },
       },
+      men: {
+        count_per_hour: {
+          6: Number,
+          7: Number,
+          8: Number,
+          9: Number,
+          10: Number,
+          11: Number,
+          12: Number,
+          13: Number,
+          14: Number,
+          15: Number,
+          16: Number,
+          17: Number,
+          18: Number,
+          19: Number,
+        },
+      },
       child: {
         count_per_hour: {
           6: Number,
