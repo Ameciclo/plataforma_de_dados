3f34fc9ca711e23893954039296c86e409738a74
Igor Matos <igormspc@gmail.com>
2020-11-27 00:02:10 -0300

    Remove comment

--- frontend/pages/contagens/[contagem].tsx
+++ frontend/pages/contagens/[contagem].tsx
@@ -234,8 +234,6 @@ export async function getStaticPaths() {
     params: { contagem: c._id },
   }));
 
-  // We'll pre-render only these paths at build time.
-  // { fallback: false } means other routes should 404.
   return { paths, fallback: false };
 }
 
