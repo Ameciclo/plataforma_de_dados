174cf51dd4913b0d648594b7df68d95ed6340b8a
Daniel <dvalenca@gmail.com>
2023-02-23 13:21:39 -0300

    removendo importações desnessárias

--- components/Breadcrumb.tsx
+++ components/Breadcrumb.tsx
@@ -1,50 +1,77 @@
 import React from "react";
 import Link from "next/link";
+import { useRouter } from "next/router";
+
+const generateBreadcrumbs = (router) => {
+  // Remove any query parameters, as those aren't included in breadcrumbs
+  const asPathWithoutQuery = router.asPath.split("?")[0];
+
+  // Break down the path between "/"s, removing empty entities
+  // Ex:"/my/nested/path" --> ["my", "nested", "path"]
+  const asPathNestedRoutes = asPathWithoutQuery.split("/")
+                                               .filter(v => v.length > 0);
+
+  // Iterate over the list of nested route parts and build
+  // a "crumb" object for each one.
+  const crumblist = asPathNestedRoutes.map((subpath, idx) => {
+    // We can get the partial nested route for the crumb
+    // by joining together the path parts up to this point.
+    const href = "/" + asPathNestedRoutes.slice(0, idx + 1).join("/");
+    // The title will just be the route string for now
+    const title = subpath;
+    return { href, title }; 
+  })
+
+  // Add in a default "Home" crumb for the top-level
+  return [{ href: "/", text: "Home" }, ...crumblist, router];
+}
 
 const Breadcrumb = ({ conf, baseItem = null }) => {
+
+  const router = useRouter();
+
   return (
-    <div className="bg-ameciclo text-white p-4 items-center uppercase flex">
-     <div className="container mx-auto">
-        <nav className="bg-grey-light rounded font-sans w-full">
-          <ol className="list-none p-0 inline-flex max-w-full">
-            {conf.routes.map((route, i) => {
-              if (route === "/") {
-                return (
-                  <BreadcrumbItem
-                    slug={baseItem.slug}
-                    label={baseItem.label}
-                    lastItem={false}
-                    key={i}
-                  />
-                );
-              }
-              if (i === conf.routes.length - 1) {
-                return (
-                  <BreadcrumbItem
-                    slug={conf.slug}
-                    label={conf.label}
-                    lastItem={true}
-                    key={i}
-                  />
-                );
-              }
+    <div className="bg-ameciclo text-white p-4 items-center uppercase w-full">
+      <nav className="bg-grey-light rounded font-sans w-full">
+        <ol className="list-none p-0 inline-flex max-w-full">
+          {conf.routes.map((route, i) => {
+            console.log(JSON.stringify(generateBreadcrumbs(router)))
+            if (route === "/") {
               return (
-                <BreadcrumbItem
-                  slug={route}
-                  label={route}
+                <Crumb
+                  slug={baseItem.slug}
+                  label={baseItem.label}
                   lastItem={false}
                   key={i}
                 />
               );
-            })}
-          </ol>
-        </nav>
+            }
+            if (i === conf.routes.length - 1) {
+              return (
+                <Crumb
+                  slug={conf.slug}
+                  label={conf.label}
+                  lastItem={true}
+                  key={i}
+                />
+              );
+            }
+            return (
+              <Crumb
+                slug={route}
+                label={route}
+                lastItem={false}
+                key={i}
+              />
+            );
+          })}
+        </ol>
+      </nav>
     </div>
-  </div>
   );
 };
 
-const BreadcrumbItem = ({ slug, label, lastItem }) => {
+const Crumb = ({ slug, label, lastItem }) => {
   return (
     <li className={`flex items-center ${lastItem ? "last-breadcrumb" : ""}`}>
       <Link href={slug}>
