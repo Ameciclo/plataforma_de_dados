c81112a4862928252ef971b938874fa74bee31eb
Daniel <dvalenca@gmail.com>
2023-03-16 12:48:20 -0300

    home page: done!

--- servers.js
+++ servers.js
@@ -1,2 +1,2 @@
-export const DATA_PARTNERS = "http://localhost:3300/data_partners"
 export const FEATURED_PAGES = "http://localhost:3300/featured_pages"
+export const PLATAFORM_HOME_PAGE = "https://cms.ameciclo.org/plataforma-de-dados"
