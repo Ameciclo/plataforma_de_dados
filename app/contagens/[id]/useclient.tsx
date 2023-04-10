a5293168d557894e167ac5c5851d73bb295bdecf
Daniel <dvalenca@gmail.com>
2023-04-10 20:39:20 -0300

    uma coluna quando pequeno

--- app/contagens/[id]/useclient.tsx
+++ app/contagens/[id]/useclient.tsx
@@ -1,5 +1,5 @@
 "use client";
-import React from "react";
+import React, { useState }  from "react";
 import HighchartsReact from "highcharts-react-official";
 import Highcharts from "highcharts";
 
