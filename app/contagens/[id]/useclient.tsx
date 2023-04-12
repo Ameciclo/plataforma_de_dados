e8cfab67ec726a365b77584b8ba9dd421cacda1a
Daniel <dvalenca@gmail.com>
2023-04-12 16:52:27 -0300

    criando página de comparação

--- app/contagens/[id]/useclient.tsx
+++ app/contagens/[id]/useclient.tsx
@@ -1,7 +1,90 @@
 "use client";
-import React, { useState }  from "react";
+import React  from "react";
 import HighchartsReact from "highcharts-react-official";
 import Highcharts from "highcharts";
+import { matchSorter } from "match-sorter";
+import Link from "next/link";
+import { ColumnFilter } from "../../components/Table/TableFilters";
+import { Table } from "../../components/Table/Table";
+
+function fuzzyTextFilterFn(rows, id, filterValue) {
+  return matchSorter(rows, filterValue, { keys: [(row) => row.values[id]] });
+}
+
+// Let the table remove the filter if the string is empty
+fuzzyTextFilterFn.autoRemove = (val) => !val;
+
+export const CountingComparisionTable = ({ data, firstId }) => {
+  const filterTypes = React.useMemo(
+    () => ({
+      // Add a new fuzzyTextFilterFn filter type.
+      fuzzyText: fuzzyTextFilterFn,
+      // Or, override the default text filter to use
+      // "startWith"
+      text: (rows, id, filterValue) => {
+        return rows.filter((row) => {
+          const rowValue = row.values[id];
+          return rowValue !== undefined
+            ? String(rowValue)
+                .toLowerCase()
+                .startsWith(String(filterValue).toLowerCase())
+            : true;
+        });
+      },
+    }),
+    []
+  );
+
+  const columns = React.useMemo(
+    () => [
+      {
+        Header: "Nome",
+        accessor: "name",
+        Cell: ({ row }) => (
+          <Link
+            className="text-ameciclo"
+            href={`contagens/${row.original._id}`}
+            key={row.original._id}
+          >
+            {row.original.name}
+          </Link>
+        ),
+        Filter: ColumnFilter,
+      },
+      {
+        Header: "Data",
+        accessor: "date",
+        Cell: ({ value }) => (
+          <span>{value.substr(0, 10).split("-").reverse().join("/")}</span>
+        ),
+        Filter: ColumnFilter,
+      },
+      {
+        Header: "Total de Ciclistas",
+        accessor: "summary.total",
+        Filter: ColumnFilter,
+        disableFilters: true,
+      },
+      {
+        Header: "COMPARE",
+        Cell: ({ row }) => (
+          <span>
+            <Link
+              className="text-ameciclo"
+              href={`/contagens/${firstId}/${row.original._id}`}
+            >
+              COMPARE
+            </Link>
+          </span>
+        ),
+        disableFilters: true,
+      },
+    ],
+    []
+  );
+
+  return <Table title={"Compare com outras contagens"} data={data} columns={columns} />;
+};
 
 type Series = {
   name: string | undefined;
