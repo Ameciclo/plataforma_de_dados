e503727e5b4773b4ccc1ca506cfab332ba1ab818
Daniel <dvalenca@gmail.com>
2023-04-13 12:22:03 -0300

    caixas de mesmo tamanho

--- app/components/VerticalStatisticsBoxes.tsx
+++ app/components/VerticalStatisticsBoxes.tsx
@@ -4,7 +4,7 @@ import { IntlDateStr, IntlNumber1Digit } from "../../utils";
 export function VerticalStatisticsBoxes({ title, boxes }) {
   return (
     <section className="container mx-auto">
-      <div className="mx-auto text-center my-24">
+      <div className="mx-auto text-center my-12">
         <h3 className="text-4xl font-bold my-8">{title}</h3>
         <section
           className={`container mx-auto grid grid-cols-1 lg:grid-cols-${boxes.length} md:grid-cols-2 auto-rows-auto gap-10 my-10`}
@@ -17,30 +17,26 @@ export function VerticalStatisticsBoxes({ title, boxes }) {
     </section>
   );
 }
-
 function VerticalBox({ titulo, media, mediaType = "", color, parametros }) {
   return (
-    <div className="rounded shadow-2xl">
+    <div className="container rounded shadow-2xl flex flex-col">
+      <div className="flex flex-col justify-center font-bold text-2xl uppercase w-full p-6 text-center tracking-widest" style={{ background: color, flex: '1' }}>
+        <h3>{titulo}</h3>
+        <h3 className="text-4xl font-bold mt-2">
+          {media &&
+            (() => {
+              switch (mediaType) {
+                case "number":
+                  return IntlNumber1Digit(media);
+                case "date":
+                  return IntlDateStr(media);
+                default:
+                  return media;
+              }
+            })()}
+        </h3>
+      </div>
       <div className="flex flex-col mx-4 md:mx-auto max-w-4xl divide-y md:divide-x divide-gray-100">
-        <div
-          className="flex flex-col justify-center font-bold text-2xl uppercase w-full p-6 text-center tracking-widest"
-          style={{ background: color }}
-        >
-          <h3>{titulo}</h3>
-          <h3 className="text-4xl font-bold mt-2">
-            {media &&
-              (() => {
-                switch (mediaType) {
-                  case "number":
-                    return IntlNumber1Digit(media);
-                  case "date":
-                    return IntlDateStr(media);
-                  default:
-                    return media;
-                }
-              })()}
-          </h3>
-        </div>
         {parametros.map((inner_param: any) => (
           <StatisticBox {...inner_param} />
         ))}
