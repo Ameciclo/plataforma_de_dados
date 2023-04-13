771675edcab7197a7fe536bfade59b798c1b9d84
Daniel <dvalenca@gmail.com>
2023-04-13 19:24:57 -0300

    troquei seta por cor, mais claro sobre o que é

--- app/components/VerticalStatisticsBoxes.tsx
+++ app/components/VerticalStatisticsBoxes.tsx
@@ -20,7 +20,10 @@ export function VerticalStatisticsBoxes({ title, boxes }) {
 function VerticalBox({ titulo, media, mediaType = "", color, parametros }) {
   return (
     <div className="container rounded shadow-2xl flex flex-col">
-      <div className="flex flex-col justify-center font-bold text-2xl uppercase w-full p-6 text-center tracking-widest" style={{ background: color, flex: '1' }}>
+      <div
+        className="flex flex-col justify-center font-bold text-2xl uppercase w-full p-6 text-center tracking-widest"
+        style={{ background: color, flex: "1" }}
+      >
         <h3>{titulo}</h3>
         <h3 className="text-4xl font-bold mt-2">
           {media &&
@@ -46,17 +49,19 @@ function VerticalBox({ titulo, media, mediaType = "", color, parametros }) {
 }
 
 function StatisticBox({ titulo, media, maior, menor }) {
+  let text_color = "text-gray-900";
+  if (!(maior & menor)) {
+    if (maior) text_color = "text-ameciclo";
+    if (menor) text_color = "text-red-500";
+  }
   return (
     <div className="flex flex-col justify-center uppercase w-full p-6 text-center tracking-widest">
       <h3>{titulo}</h3>
-      <h3 className="text-4xl font-bold mt-2">
+      <h3 className={`text-4xl font-bold mt-2 ${text_color}`}>
         {/* {media !== null && media >= 0
           ? IntlNumber1Digit(media) + (different ? (bigger ? "" : "") : "")
           : "N/A"} */}
-        {media !== null
-          ? media +
-            (!(maior & menor) ? (maior ? "🔺" : "") + (menor ? "🔻" : "") : "")
-          : "N/A"}
+        {media !== null ? media : "N/A"}
       </h3>
     </div>
   );
