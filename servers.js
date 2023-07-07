b04db6f9c386cc0d3b1f60a95e6e4c16f0f9365e
Daniel <dvalenca@gmail.com>
2023-07-07 15:48:06 -0300

    criada nova api da contagem

--- servers.js
+++ servers.js
@@ -1,6 +1,7 @@
 export const PLATAFORM_HOME_PAGE = "https://cms.ameciclo.org/plataforma-de-dados"
 export const DOCUMENTS_PAGE = "https://cms.ameciclo.org/documentos"
 export const DOCUMENTS_DATA = "https://cms.ameciclo.org/documents"
+//export const COUNTINGS_SUMMARY_DATA =`https://api.contagem.ameciclo.org/v1/cyclist-count/metadata`
 export const COUNTINGS_SUMMARY_DATA =`https://api.contagem.ameciclo.org/v1/cyclist-count/metadata`
 export const COUNTINGS_DATA =`https://api.contagem.ameciclo.org/v1/cyclist-count/`
 export const COUNTINGS_PAGE_DATA =`https://cms.ameciclo.org/contagens`
