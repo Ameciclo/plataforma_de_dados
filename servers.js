8a0663666a1da18c87704f22801851a42a1bc30e
Daniel Valença <dvalenca@gmail.com>
2024-01-26 12:36:10 -0300

    servidor temporário

--- servers.js
+++ servers.js
@@ -10,11 +10,11 @@ export const IDECICLO_STRUCTURES_DATA = `https://api.ideciclo.ameciclo.org/struc
 export const IDECICLO_FORMS_DATA = `https://api.ideciclo.ameciclo.org/forms`
 export const PERFIL_DATA = `https://api.perfil.ameciclo.org/v1/cyclist-profile/summary/`
 
-export const COUNTINGS_SUMMARY_DATA =`http://localhost:8080/cyclist-counts`     
-export const COUNTINGS_DATA =`http://localhost:8080/cyclist-counts/edition`
+export const COUNTINGS_SUMMARY_DATA =`http://135.148.32.138:3563/cyclist-counts`     
+export const COUNTINGS_DATA =`http://135.148.32.138:3563/cyclist-counts/edition`
 
-export const OBSERVATORY_DATA =`http://localhost:8080/cyclist-infra/relationsByCity`
-export const OBSERVATORY_DATA_WAYS =`http://localhost:8080/cyclist-infra/ways`
-export const OBSERVATORY_DATA_ALL_WAYS = `http://localhost:8080/cyclist-infra/ways/all-ways`
-export const OBSERVATORY_DATA_WAYS_SUMMARY =`http://localhost:8080/cyclist-infra/ways/summary`
-export const CITIES_DATA =`http://localhost:8080/cities`
+export const OBSERVATORY_DATA =`http://135.148.32.138:3563/cyclist-infra/relationsByCity`
+export const OBSERVATORY_DATA_WAYS =`http://135.148.32.138:3563/cyclist-infra/ways`
+export const OBSERVATORY_DATA_ALL_WAYS = `http://135.148.32.138:3563/cyclist-infra/ways/all-ways`
+export const OBSERVATORY_DATA_WAYS_SUMMARY =`http://135.148.32.138:3563/cyclist-infra/ways/summary`
+export const CITIES_DATA =`http://135.148.32.138:3563/cities`
